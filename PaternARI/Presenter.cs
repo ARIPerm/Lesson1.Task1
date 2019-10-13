e4c01ef390a52943a1eb19dc3a955f1250d4f5df
Angelina Istomina <isanro@mail.ru>
2019-10-13 10:11:06 +0500

    Одномерный массив

---
+++ PaternARI/Presenter.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaternARI
+{
+    public class Presenter
+    {
+        private static int START_VALUE = 5;
+        static Random rnd = new Random();
+        public int[] array;
+
+        public Presenter()
+        {
+            array = new int[START_VALUE];
+            for (int i = 0; i < START_VALUE; i++)
+            {
+                array[i] = rnd.Next(-10, 10);
+            }
+        }
+
+        public void Add(int item)
+        {
+            Array.Resize(ref array, array.Length + 1);
+            array[array.Length - 1] = item;
+        }
+
+        public void Remove(int index)
+        {
+            for (int i = index - 1; i < array.Length - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+            Array.Resize(ref array, array.Length - 1);
+        }
+
+        public void Delete2n()
+        {
+            int temp;
+            bool cout = true;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if ((array[i] % 2) == 0 && array[i] != 0 && cout)
+                {
+                    cout = false;
+                    for (int k = i; k < array.Length - 1; k++)
+                    {
+                        temp = array[k];
+                        array[k] = array[k + 1];
+                        array[k + 1] = temp;
+                    }
+                    Array.Resize(ref array, array.Length - 1);
+                }
+            }
+            if (cout)
+            {
+                Console.WriteLine("У вас нет четных чисел");
+            }
+        }
+
+        public void Sort()
+        {
+            int temp;
+            for (int i = 0; i < array.Length - 2; i++)
+            {
+                if (i % 2 == 0 || i == 0)
+                {
+                    if (array[i] > array[i + 2])
+                    {
+                        temp = array[i];
+                        array[i] = array[i + 2];
+                        array[i + 2] = temp;
+                    }
+                }
+            }
+        }
+
+    }
+
+}
---
+++ PaternARI/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaternARI
+{
+    class Program
+    {
+        static Presenter presenter = new Presenter();
+
+        static void Main(string[] args)
+        {
+            Print();
+            for (; ; )
+            {
+                Console.WriteLine("Выберите операцию:\n" +
+                    " 1-Добавить элемент\n" +
+                    " 2-Удалить элемент\n" +
+                    " 3-Вывести массив\n" +
+                    " 4-Задание по варианту\n" +
+                    " 5-Удалить первый четный элемент\n");
+                int x;
+                string k = Console.ReadLine();
+                if (Int32.TryParse(k, out x))
+                {
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод");
+                }
+                switch (x)
+                {
+                    case 1:
+                        Console.WriteLine("Введите число:");
+                        int value;
+                        string s = Console.ReadLine();
+                        if (Int32.TryParse(s, out value))
+                        {
+                            presenter.Add(value);
+                            Print();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Некорректный ввод");
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Какой элемент удалить?");
+                        int del;
+                        string k1 = Console.ReadLine();
+                        if (Int32.TryParse(k1, out del))
+                        {
+                            presenter.Remove(del);
+                            Print();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Некорректный ввод");
+                        }
+                        break;
+                    case 3:
+                        Print();
+                        break;
+                    case 4:
+                        presenter.Sort();
+                        Print();
+                        break;
+                    case 5:
+                        presenter.Delete2n();
+                        Print();
+                        break;
+                    default:
+
+                        Console.WriteLine("Пользователь дурачок");
+                        break;
+                }
+            }
+        }
+        public static void Print()
+        {
+            foreach (int value in presenter.array)
+            {
+                Console.Write($" {value} ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+
+}
